2b3ce04f11786b7e18740efcca7bd63e9305b570
luisbia <luisbia@gmail.com>
2022-11-13 23:46:26 +0100

    correct roxygen

--- R/show_DT.R
+++ R/show_DT.R
@@ -5,7 +5,7 @@
 #' An interactive HTML table with some fancy options (filter,sort,export to excel) is generated from a data frame.
 #'
 #' @param x a data frame
-#' @caption a caption text for the table
+#' @param caption  a caption text for the table
 #'
 #' @return DT table
 #' @export show_DT
