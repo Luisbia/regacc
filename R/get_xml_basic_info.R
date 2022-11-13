e6a10a7c1d26edf15df5f13384902d34374ece3d
luisbia <luisbia@gmail.com>
2022-11-13 23:34:05 +0100

    fix stuff

--- R/get_xml_basic_info.R
+++ R/get_xml_basic_info.R
@@ -12,71 +12,48 @@
 #' time_min = "2021-10-01")
 #' 
 get_xml_basic_info<- function(...){
+  regacc::check_packages()
+  data<-regacc::load_xml(...) %>% 
+    as.data.table
   
-  files<-regacc::load_xml(...)
-  
-  temp<- files %>% 
-    group_by(table,sto,activity,unit_measure,accounting_entry) %>% 
-    select(value,country,time_period,sto,activity,unit_measure,accounting_entry,obs_value,obs_status) %>% 
-    drop_na(obs_value) %>% 
-    filter(!obs_status %in% c("M", "L")) %>% 
-    mutate(first=min(time_period),
-           last=max(time_period)) %>% 
-    ungroup() %>% 
-    select(value,country,table,sto,activity,unit_measure,accounting_entry,first,last) %>% 
-    unique()
+  temp<- unique(data[!obs_status %in% c("L","M") & !is.na(obs_value) ,.(first = min(time_period), last = max(time_period)),by =.(table_identifier,country,sto,accounting_entry,unit_measure)])%>% 
+    .[,lapply(.SD, as.factor),] %>% # convert to factors for better filtering
+    droplevels()
+
   
   temp1<-show_DT(temp, caption ="Reported periods")
   
   
-temp<- files %>% 
-  select(value,country,table,ref_area,time_period,sto,activity,unit_measure,accounting_entry,obs_value) %>% 
-  na.omit() 
+  if ("embargo_date" %in% colnames(data)){
+    temp<- na.omit(unique(files[,.(table_identifier,country,embargo_date)])) %>% 
+      .[,lapply(.SD, as.factor),] %>% 
+      droplevels()
 
-NUTS <- dataregacc::NUTS_2021 %>% 
-  select(-label)
+    temp2<-show_DT(temp, caption ="Embargo dates")
+  } else {
+    temp2<- "No embargo dates"
+  }
 
-temp<- left_join(temp,NUTS) %>%
-  select(value,table,country,NUTS,time_period,sto,activity,unit_measure,accounting_entry) %>% 
-  group_by(value,table,country,NUTS,sto,unit_measure) %>% 
-  count()
-
-temp2<-show_DT(temp, caption = "Reported ref_area")
-
-temp<- files
-
-if ("embargo_date" %in% colnames(temp)){
-  temp<- temp %>% 
-    select(value,country,table,ref_area,time_period,sto,activity,unit_measure,accounting_entry,obs_value, obs_status,conf_status,embargo_date) %>% 
-    filter(conf_status !="F") %>% 
-    na.omit() %>%  
-    group_by(value,table,country,time_period,obs_status, conf_status,embargo_date) %>% 
-    count()
+  if ("comment_ts" %in% names(df)) {
+    temp<-na.omit(unique(data[comment_ts!="xxx" & comment_ts!="",.(country,table_identifier,comment_ts)])) %>%   
+      .[,lapply(.SD, as.factor),] %>% 
+      droplevels()
+    temp3<-show_DT(temp, caption ="Comments")
+  } else {
+    temp2<- "No comments"
+  }
+    
+  temp<-na.omit(unique(data[conf_status=="N",.(country,table_identifier,sto,activity,unit_measure,conf_status)]))%>% 
+    .[,lapply(.SD, as.factor),] %>% 
+    droplevels()
+  temp3<-show_DT(temp, caption ="Data not publishable")
   
-} else {
-  temp<- temp %>% 
-    select(value,table,country,time_period,sto,activity,unit_measure,accounting_entry,obs_status,conf_status) %>% 
-    group_by(value,table,country,time_period,obs_status, conf_status) %>% 
-    count()
+  temp<-na.omit(unique(data[obs_status %in% c("E","P","B","D","U"),.(country,table_identifier,sto,unit_measure,obs_status,time_period)])) %>% 
+    .[,lapply(.SD, as.factor),] %>% 
+    droplevels()  
   
-}
-temp3<-show_DT(temp, caption = "Reported flags")
-
-temp<- files %>%
-  ungroup() 
-
-if ("comment_ts" %in% colnames(files)){
-  temp<- temp %>% 
-    select(value,country,table,sto,unit_measure,comment_ts) %>%
-    na.omit() %>% 
-    unique()
+  temp4<-show_DT(temp, caption ="Flags")
   
-  temp4<-show_DT(temp, caption = "Comments")
-  
-} else {
-  print("No comments")
-}
-
 return(list(temp1,temp2,temp3,temp4))
 }
 
