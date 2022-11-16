28696047fc3060fb781578cf8e666bdbbe243397
luisbia <luisbia@gmail.com>
2022-11-16 22:04:17 +0100

    theme and palette changes

--- R/ra_palettes.R
+++ R/ra_palettes.R
@@ -13,8 +13,8 @@
 ra_palettes <- function(name, n, all_palettes = ra_colours, type = c("discrete", "continuous")) {
 
   ra_colours = list(
-    light = c("#2644A7", "#AF155C", "#208486", "#AA5F18", "#B656BD","#388AE2","#E04040", "#33A033","#672DC4","#B39421"),
-    dark = c("#588CFB", "#D3659F","#4B8C8A","#B06923", "#B656BD", "#75BDFC","#E65050","#73C472","#8E61F8","#D8AC28"),
+    light = c("#2644A7", "#AF155C", "#208486", "#B39421", "#B656BD","#388AE2","#E04040", "#33A033","#672DC4","#AA5F18"),
+    dark = c("#588CFB", "#D3659F","#4B8C8A","#D8AC28", "#B656BD", "#75BDFC","#E65050","#73C472","#8E61F8" ,"#B06923"),
     primary = c("#0E47CB","#FFCC00"),
     monoblue = c("#082B7A", "#0B39A2" , "#0E47CB", "#6E91E0", "#CFDAF5"),
     monofuchsia = c("#241125", "#6D3371", "#B656BD", "#D399D7", "#F0DDF1" )
--- R/theme_ra.R
+++ R/theme_ra.R
@@ -1,6 +1,7 @@
 #' Default theme options for regacc charts
 #'
 #' @param font Font to be used by default MS Sans Sheriff
+#' @param fontsize 12 by default
 #' @return a set of theme paremeters
 #' @export theme_ra
 #'
@@ -16,94 +17,68 @@
 #'   scale_colour_ra()+
 #'   labs(title="Greece NUTS 1 regions GDP",
 #'        subtitle="EUR per inhabitant")
-theme_ra <- function(font= "MS Sans Serif") {
+theme_ra <- function(font= "MS Sans Serif",fontsize=12) {
   library(ragg)
   font <- font
-
-  ggplot2::theme(
-
-    # TEXT FORMAT
-    # This sets the font, size, type and colour
-    # of text for the chart's title
-    plot.title = ggplot2::element_text(
-      family = font,
-      size = 20,
-      face = "bold",
-      color = "#262B38"
-    ),
-    # This sets the font, size, type and colour
-    # of text for the chart's subtitle,
-    # as well as setting a margin between the title and the subtitle
-    plot.subtitle = ggplot2::element_text(
-      family = font,
-      color = "#262B38",
-      size = 16,
-      margin = ggplot2::margin(9, 0, 9, 0)
-    ),
-    # This leaves the caption text element empty,
-    # because it is set elsewhere in the finalise plot function
-    plot.caption = ggplot2::element_blank(),
-
-    # LEGEND FORMAT
-    # This sets the position and alignment of the legend,
-    # removes a title and background for it
-    # and sets the requirements for any text within the legend.
-    # The legend may often need some more manual tweaking
-    # when it comes to its exact position based on the plot coordinates.
-    legend.position = "top",
-    legend.text.align = 0,
-    legend.background = ggplot2::element_blank(),
-    legend.title = ggplot2::element_blank(),
-    legend.key = ggplot2::element_blank(),
-    legend.text = ggplot2::element_text(
-      family = font,
-      size = 12,
-      color = "#262B38"
-    ),
-
-    # AXIS FORMAT
-    # This sets the text font, size and colour for the axis test,
-    # as well as setting the margins and removes lines and ticks.
-    # In some cases, axis lines and axis ticks are things we would
-    # want to have in the chart -
-    # the cookbook shows examples of how to do so.
-    axis.title = ggplot2::element_blank(),
-    axis.text = ggplot2::element_text(
-      family = font,
-      size = 12,
-      color = "#262B38"
-    ),
-    axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10),family=font),
-    axis.ticks = ggplot2::element_blank(),
-    axis.line = ggplot2::element_line(colour="#262B38"),
-
-    # GRID LINES
-    # This removes all minor gridlines and adds major y gridlines.
-    # In many cases you will want to change this to remove
-    # y gridlines and add x gridlines.
-    # The cookbook shows you examples for doing so.
-    panel.grid.minor = ggplot2::element_blank(),
-    panel.grid.major= ggplot2::element_line(color = "#D4D5D7"),
-
-    # BLANK BACKGROUND
-    # This sets the panel background as blank, removing the standard
-    # grey ggplot background colour from the plot.
-    panel.background = ggplot2::element_rect(fill = "#F3F6FC"),
-
-    plot.background = element_rect(fill= "#FFFFFF",
-                                   colour = "#262B38"),
-    line = element_line(colour = "#262B38"),
-    rect = element_rect(fill = "#FAFAFA",
-                        linetype = 0,
-                        colour = NA),
-
-    # STRIP BACKGROUND
-    # This sets the panel background for facet-wrapped plots to white,
-    # removing the standard grey ggplot background colour and sets the
-    # title size of the facet-wrap title to font size 22.
-    strip.background = ggplot2::element_rect(fill = "#F3F6FC"),
-    strip.text = ggplot2::element_text(size = 16, hjust = 0.1, color="#262B38",face="bold", family=font),
-    panel.spacing = unit(2, "lines"),
-    strip.placement = "outside"
-  )
+  dark_text <- "#262B38"
+  mid_text <- "#51555F"
+  light_text<- "#7C7F87"
+  
+  ggplot2::theme_minimal(base_size = fontsize)+
+    ggplot2::theme(
+      text = element_text(colour = mid_text, family = font, lineheight = 1.1),
+      plot.title = ggplot2::element_text(
+        family = font,
+        size = rel(1.6),
+        face = "bold",
+        color = dark_text,
+        margin = margin(12, 0, 8, 0)),
+      plot.subtitle = ggplot2::element_text(
+        family = font,
+        color = dark_text,
+        size = rel(1.1),
+        margin = ggplot2::margin(5, 0, 5, 0)
+      ),
+      plot.caption = ggplot2::element_blank(),
+      
+      legend.position = "top",
+      legend.text.align = 0,
+      legend.justification = 1,
+      legend.background = ggplot2::element_blank(),
+      legend.title = ggplot2::element_blank(),
+      legend.key = ggplot2::element_blank(),
+      legend.text = ggplot2::element_text(
+        family = font,
+        size = rel(1.0),
+        color = light_text
+      ),
+      
+      axis.title = ggplot2::element_blank(),
+      axis.text = ggplot2::element_text(
+        family = font,
+        size = rel(1.0),
+        color = mid_text
+      ),
+      axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10),family=font,colour = light_text),
+      axis.ticks = ggplot2::element_blank(),
+      axis.line = ggplot2::element_line(colour=light_text),
+      
+      
+      panel.grid.minor = ggplot2::element_blank(),
+      panel.grid.major= ggplot2::element_line(color = "#D4D5D7"),
+      
+      panel.background = ggplot2::element_rect(fill = "#F3F6FC"),
+      
+      plot.background = element_rect(fill= "#FFFFFF",
+                                     colour = light_text),
+      line = element_line(colour = light_text),
+      rect = element_rect(fill = "#FAFAFA",
+                          linetype = 0,
+                          colour = NA),
+      
+      strip.background = ggplot2::element_rect(fill = "#F3F6FC"),
+      strip.text = ggplot2::element_text(size = rel(1.4), hjust = 0.1, color=mid_text,face="bold", family=font),
+      panel.spacing = unit(2, "lines"),
+      strip.placement = "outside"
+    )
 }
